556e0a5f30ebd0397ad8f6819b059b06c4a49d2a
yomna tara <yomnahtara@gmail.com>
2020-04-03 22:29:17 +0200

    first add my project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function turn(x){
+ if(x=='on'){
+     document.getElementById("lamb").src="images/pic_bulbon.gif";
+   document.getElementById("demo").style.backgroundColor = "#009944"; }
+   else{
+    document.getElementById("lamb").src="images/pic_bulboff.gif";
+    document.getElementById("demo").style.backgroundColor = "#876";
+
+   }
+
+}
